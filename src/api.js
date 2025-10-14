6792bf70af43ad1b3a47d9edb521907e8e02c8cc
Klodi <mekshiklodi10@gmail.com>
2025-10-14 20:38:11 +0200

    NDRYSHIM

--- src/api.js
+++ src/api.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", 
+  baseURL: "https://backend-delta-weld-98.vercel.app/api", 
 });
 
 export const registerUser = (formData) => API.post("/auth/register", formData);
-
 export const loginUser = (formData) => API.post("/auth/login", formData);
